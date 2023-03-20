3d2012f3631baa185ab3d46485133fae986cb509
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2023-03-20 13:39:51 +0000

    Adds analyse_read_lengths.py

---
+++ analyse_read_lengths.py
@@ -0,0 +1,130 @@
+import gzip
+from pathlib import Path
+import argparse
+import os, glob
+from collections import defaultdict
+
+def get_options():
+    description = "Parses reads based on lengths"
+    parser = argparse.ArgumentParser(description=description,
+                                     prog='python analyse_read_lengths.py')
+    IO = parser.add_argument_group('Input/options.out')
+    IO.add_argument('--indir',
+                    required=True,
+                    help='Path to read directory')
+    IO.add_argument('--out',
+                    default="result.txt",
+                    help='Output file.')
+    return parser.parse_args()
+
+def get_fq(directory):
+    types = ([".fastq"], [".fastq", ".gz"], [".fq"], [".fq", ".gz"])
+    files = (
+        str(p.resolve()) for p in Path(directory).glob("**/*") if p.suffixes in types
+    )
+    yield from files
+
+def readfq(fp):  # this is a generator function
+    """Read FASTA/Q records from file handle
+    https://github.com/lh3/readfq/blob/091bc699beee3013491268890cc3a7cbf995435b/readfq.py
+    """
+    last = None  # this is a buffer keeping the last unprocessed line
+    while True:  # mimic closure; is it a bad idea?
+        if not last:  # the first record or a record following a fastq
+            for l in fp:  # search for the start of the next record
+                if l[0] in ">@":  # fasta/q header line
+                    last = l[:-1]  # save this line
+                    break
+        if not last:
+            break
+        name, seqs, last = last[1:].partition(" ")[0], [], None
+        for l in fp:  # read the sequence
+            if l[0] in "@+>":
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != "+":  # this is a fasta record
+            yield name, "".join(seqs), None  # yield a fasta record
+            if not last:
+                break
+        else:  # this is a fastq record
+            seq, leng, seqs = "".join(seqs), 0, []
+            for l in fp:  # read the quality
+                seqs.append(l[:-1])
+                leng += len(l) - 1
+                if leng >= len(seq):  # have read enough quality
+                    last = None
+                    yield name, seq, "".join(seqs)
+                    # yield a fastq record
+                    break
+            if last:  # reach EOF before reading enough quality
+                yield name, seq, None  # yield a fasta record instead
+                break
+
+def main():
+    options = get_options()
+    out = options.out
+    indir = options.indir
+
+    target_reads_dict = defaultdict(list)
+    unblocks_reads_dict = defaultdict(list)
+    other_reads_dict = defaultdict(list)
+
+    sum_list = glob.glob(os.path.join(indir, "sequencing_summary_*.txt"))
+    summary = sum_list[0]
+    unblock_dict = {}
+
+    with open(summary, "r") as f:
+        # ignore header
+        next(f)
+        for line in f:
+            entry = line.strip().split("\t")
+            read_id = entry[4]
+            unblock = entry[23]
+            unblock_dict[read_id] = unblock
+
+
+    for f in get_fq(indir):
+        if f.endswith(".gz"):
+            fopen = gzip.open
+        else:
+            fopen = open
+
+        base = os.path.splitext(os.path.basename(f))[0].split("_")
+        if "barcode" in base[2]:
+            file_id = "_".join([base[1], base[2]])
+        else:
+            file_id = "_".join([base[1], "NA"])
+
+
+        with fopen(f, "rt") as fh:
+            for name, seq, _ in readfq(fh):
+
+                if name in unblock_dict:
+                    if unblock_dict[name] == "signal_positive":
+                        target_reads_dict[file_id].append((name, len(seq)))
+                    elif unblock_dict[name] == "data_service_unblock_mux_change":
+                        unblocks_reads_dict[file_id].append((name, len(seq)))
+                    else:
+                        other_reads_dict[file_id].append((name, len(seq)))
+                else:
+                    other_reads_dict[file_id].append((name, len(seq)))
+
+    with open(out, "w") as o:
+        o.write("Type\tFilter\tBarcode\tName\tLength\n")
+        for file_id, length_list in target_reads_dict.items():
+            type = file_id.split("_")
+            for len_entry in length_list:
+                o.write("Target\t" + type[0] + "\t" + type[1] + "\t" + str(len_entry[0]) + "\t" + str(len_entry[1]) + "\n")
+        for file_id, length_list in unblocks_reads_dict.items():
+            type = file_id.split("_")
+            for len_entry in length_list:
+                o.write("Non-target\t" + type[0] + "\t" + type[1] + "\t" + str(len_entry[0]) + "\t" + str(len_entry[1]) + "\n")
+        for file_id, length_list in other_reads_dict.items():
+            type = file_id.split("_")
+            for len_entry in length_list:
+                o.write("Other\t" + type[0] + "\t" + type[1] + "\t" + str(len_entry[0]) + "\t" + str(len_entry[1]) + "\n")
+
+
+if __name__ == "__main__":
+    main()
